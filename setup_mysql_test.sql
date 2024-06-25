44aca2a42a419912ab2e2afca53810f31ea74019
danimohli <danimohlife@gmail.com>
2024-06-25 02:24:20 +0100

    setup

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# MySQL credentials
 MYSQL_USER=root
 MYSQL_PASSWORD=root_password
 
-# Create a temporary SQL script file
 SQL_SCRIPT=$(mktemp)
 
-# Write the SQL commands to the temporary script file
 cat <<EOF > $SQL_SCRIPT
 -- Create database hbnb_test_db if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
@@ -24,11 +21,3 @@ GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
 EOF
-
-# Execute the SQL script
-mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" < $SQL_SCRIPT
-
-# Remove the temporary SQL script file
-rm $SQL_SCRIPT
-
-echo "MySQL setup completed."
