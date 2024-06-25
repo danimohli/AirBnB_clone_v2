f9438785be41e0108a5f5a2d02d253192fb0340b
danimohli <danimohlife@gmail.com>
2024-06-26 00:40:42 +0100

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,24 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
+#!/usr/bin/python
+"""
+Class Amenity
+"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """
-    Amenity class that inherits from BaseModel.
+    Representation of Amenity
     """
-    name = ""
+    if models.storage_t == 'db':
+        __tablename__ = 'amenities'
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
