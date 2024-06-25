7808159732b67986862b7320a55fe97f1fa42fc3
danimohli <danimohlife@gmail.com>
2024-06-25 03:21:13 +0100

    storage that work with mysql

---
+++ models/engine/db_storage.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+"""
+Contains the DBStorage class
+"""
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+import os
+
+
+class DBStorage:
+    """
+    Database storage engine
+    """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """
+        Initialize the database connection
+        """
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            os.getenv('HBNB_MYSQL_USER'),
+            os.getenv('HBNB_MYSQL_PWD'),
+            os.getenv('HBNB_MYSQL_HOST'),
+            os.getenv('HBNB_MYSQL_DB')
+        ), pool_pre_ping=True)
+
+        if os.getenv('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """
+        Query on the current database session
+        """
+        if cls:
+            objs = self.__session.query(cls).all()
+        else:
+            objs = []
+            for class_name in ['User', 'State', 'City',
+                               'Amenity', 'Place', 'Review']:
+                objs.extend(self.__session.query(eval(class_name)).all())
+        return {"{}.{}".format(type(obj).__name__,
+                               obj.id): obj for obj in objs}
+
+    def new(self, obj):
+        """
+        Add the object to the current database session
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """
+        Commit all changes of the current database session
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """
+        Delete from the current database session obj if not None
+        """
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """
+        Create all tables in the database and create the session
+        """
+        from models.base_model import Base
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        self.__session = scoped_session(session_factory)()
