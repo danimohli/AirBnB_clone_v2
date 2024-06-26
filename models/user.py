357e4850b5ab898d5a21ee7dfc8773b55daddb17
danimohli <danimohlife@gmail.com>
2024-06-26 15:58:56 +0100

    user

--- models/user.py
+++ models/user.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+"""
+User module
+"""
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """
-    User class that inherits from BaseModel.
+    User class that inherits from BaseModel and Base.
     """
+    __tablename__ = 'users'
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user", cascade="all, delete, delete-orphan")
