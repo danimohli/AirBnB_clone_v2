77817bfdbd0a07cf7f74f0964cd965942f80510f
danimohli <danimohlife@gmail.com>
2024-06-26 15:45:44 +0100

    base update

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,21 @@ Custom base class for the entire project
 
 from uuid import uuid4
 from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 import models
+
+Base = declarative_base()
 
 
 class BaseModel:
     """
     Basemodel for all the classes
     """
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Public instance artributes initialization
@@ -22,15 +30,20 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
-            models.storage.new(self)
         else:
             for k, v in kwargs.items():
                 if k in ("updated_at", "created_at"):
                     self.__dict__[k] = datetime.strptime(v, dtf)
-                elif k[0] == "id":
+                elif k == "id":
                     self.__dict__[k] = str(v)
                 else:
                     self.__dict__[k] = v
+            if "id" not in kwargs:
+                self.id = str(uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.utcnow()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.utcnow()
 
     def __str__(self):
         """
@@ -45,6 +58,7 @@ class BaseModel:
         with the current datetime
         """
         self.updated_at = datetime.utcnow()
+        models.storage.new(self)  # moved here from __init__
         models.storage.save()
 
     def to_dict(self):
@@ -56,7 +70,13 @@ class BaseModel:
         for k, v in self.__dict__.items():
             if k == "created_at" or k == "updated_at":
                 obj_m[k] = v.isoformat()
-            else:
+            elif k != "_sa_instance_state":
                 obj_m[k] = v
         obj_m["__class__"] = self.__class__.__name__
         return obj_m
+
+    def delete(self):
+        """
+        Delete the current instance from the storage
+        """
+        models.storage.delete(self)
