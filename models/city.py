bd4a02e15e27d55ab1d0e47f6a099bdac74dadf7
danimohli <danimohlife@gmail.com>
2024-06-26 03:31:24 +0100

    combine o/n

--- models/city.py
+++ models/city.py
@@ -1,16 +1,31 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """
-Contains the City class
+Contain City Class
 """
-
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """
     The city class, contains state ID and name
     """
-    __tablename__ = 'cities'
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """
+        initialize city
+        """
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="cities")
+    else:
+        state_id = ""
+        name = ""
