b03748ae44ca35b92c799659a02dcdaba2c1c4c6
danimohli <danimohlife@gmail.com>
2024-06-26 15:50:43 +0100

    state update

--- models/state.py
+++ models/state.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """
-    State class that inherits from BaseModel.
+    State class that inherits from BaseModel and Base.
     """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+
+    @property
+    def cities(self):
+        """
+        Getter attribute in case of FileStorage
+        """
+        return [city for city in models.storage.all(City).values() if city.state_id == self.id]
