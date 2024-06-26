ccf92049e08b6cdd7cf215d59cf6fb246bf4aa0b
danimohli <danimohlife@gmail.com>
2024-06-26 02:58:41 +0100

    citystate

--- models/state.py
+++ models/state.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+"""
+Contains the State class
+"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+import models
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """
-    State class that inherits from BaseModel.
+    State class
     """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete,
+                          delete-orphan", backref="state")
+
+    @property
+    def cities(self):
+        """
+        Getter attribute cities that returns the list of City instances
+        """
+        if models.storage_t == 'db':
+            return self.cities
+        else:
+            return [city for city in models.storage.all(City).values()
+                    if city.state_id == self.id]
