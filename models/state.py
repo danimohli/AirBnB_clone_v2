4d418875fdd5a29ef8864bd6509c6c806998bdfe
danimohli <danimohlife@gmail.com>
2024-06-26 03:40:37 +0100

    rejoin o/w

--- models/state.py
+++ models/state.py
@@ -1,30 +1,42 @@
 #!/usr/bin/python3
+""" 
+Contains State Class
 """
-Contains the State class
-"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
 import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """
-    State class
+    State Class
     """
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete,
-                          delete-orphan", backref="state")
+    def __init__(self, *args, **kwargs):
+        """
+        initialize state
+        """
+        super().__init__(*args, **kwargs)
 
-    @property
-    def cities(self):
-        """
-        Getter attribute cities that returns the list of City instances
-        """
-        if models.storage_t == 'db':
-            return self.cities
-        else:
-            return [city for city in models.storage.all(City).values()
-                    if city.state_id == self.id]
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """
+            Getter attribute cities that returns the list of City instances
+            """
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
