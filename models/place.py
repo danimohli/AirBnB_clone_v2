9603956f2121e3fd8fb3f8aefe9b8bc921dc9ef0
danimohli <danimohlife@gmail.com>
2024-06-26 21:53:29 +0100

    place

--- models/place.py
+++ models/place.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Place module
+Place Module for the project
 """
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
 
@@ -11,7 +12,6 @@ class Place(BaseModel, Base):
     Place class that inherits from BaseModel and Base.
     """
     __tablename__ = 'places'
-
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
@@ -22,3 +22,17 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
+    reviews = relationship("Review", backref="place", cascade="all, delete, delete-orphan")
+
+    @property
+    def reviews(self):
+        """
+        Returns the list of Review instances with
+        place_id equals to the current Place.id
+        """
+        from models import storage
+        review_list = []
+        for review in storage.all(Review).values():
+            if review.place_id == self.id:
+                review_list.append(review)
+        return review_list
