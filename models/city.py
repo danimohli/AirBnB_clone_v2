214ac71c4f43b44d4146574c8fc5555cd7b8fb11
danimohli <danimohlife@gmail.com>
2024-06-26 02:54:49 +0100

    citywithId

--- models/city.py
+++ models/city.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+"""
+Contains the City class
+"""
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
-class City(BaseModel):
+class City(BaseModel, Base):
     """
-    City class that inherits from BaseModel.
+    The city class, contains state ID and name
     """
-    state_id = ""
-    name = ""
+    __tablename__ = 'cities'
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
