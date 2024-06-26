01845c436f4b4287736ed17788ec895beeff601b
danimohli <danimohlife@gmail.com>
2024-06-26 22:03:37 +0100

    revi

--- models/review.py
+++ models/review.py
@@ -1,16 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """
-Review Module for the project
+Review Module for class Project
 """
-from sqlalchemy import Column, String, ForeignKey
+import models
 from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
     """
-    Review class that inherits from BaseModel and Base.
+    Review Class that inherits from Base and BaseModel
     """
-    __tablename__ = 'reviews'
-    text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    if models.storage_t == 'db':
+        __tablename__ = 'reviews'
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
+
+    def __init__(self, *args, **kwargs):
+        """
+        init funct
+        """
+        super().__init__(*args, **kwargs)
