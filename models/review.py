d03fa968cb57ee119b64c1c363432642afecfbe5
danimohli <danimohlife@gmail.com>
2024-06-27 04:06:20 +0100

    revw

--- models/review.py
+++ models/review.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+from sqlalchemy import Column, String, ForeignKey
+from models.base_model import BaseModel, Base
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """
-    Review class that inherits from BaseModel.
+    Review class that inherits from BaseModel and Base.
     """
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = 'reviews'
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
