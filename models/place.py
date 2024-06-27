7cc2fdb417f56c0ac110f310e415167fb877c9bd
danimohli <danimohlife@gmail.com>
2024-06-27 03:35:32 +0100

    place

--- models/place.py
+++ models/place.py
@@ -2,9 +2,18 @@
 """
 Place module
 """
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+
+# Define the Many-to-Many relationship table
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -25,6 +34,8 @@ class Place(BaseModel, Base):
     longitude = Column(Float, nullable=True)
     reviews = relationship("Review", backref="place",
                            cascade="all, delete, delete-orphan")
+    amenities = relationship('Amenity', secondary=place_amenity,
+                             viewonly=False, back_populates='place_amenities')
 
     @property
     def reviews(self):
@@ -38,3 +49,23 @@ class Place(BaseModel, Base):
             if review.place_id == self.id:
                 review_list.append(review)
         return review_list
+
+    @property
+    def amenities(self):
+        """
+        Getter for amenities in FileStorage
+        """
+        from models import storage
+        amenity_list = []
+        for amenity in storage.all(Amenity).values():
+            if amenity.id in self.amenity_ids:
+                amenity_list.append(amenity)
+        return amenity_list
+
+    @amenities.setter
+    def amenities(self, obj):
+        """
+        Setter for amenities in FileStorage
+        """
+        if type(obj) == Amenity:
+            self.amenity_ids.append(obj.id)
