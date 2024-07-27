cf3b7a6ce36bcc2d18e26e6556477e66f505ea0e
danimohli <danimohlife@gmail.com>
2024-07-27 23:06:06 +0100

    odd

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -25,38 +25,49 @@ def hbnb():
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
+    """
+    display “C ” followed by the value of the text variable
+    """
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
+    """
+    display “Python ”, followed by the value of the text variable
+    """
     return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def imanumber(n):
-    """display “n is a number” only if n is an integer"""
+    """
+    display “n is a number” only if n is an integer
+    """
     return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numbersandtemplates(n):
-    """display a HTML page only if n is an integer"""
+    """
+    display a HTML page only if n is an integer
+    """
     return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def numbersandevenness(n):
-    """display a HTML page only if n is an integer"""
+    """
+    display a HTML page only if n is an integer
+    """
+    stat = 'odd'
+
     if n % 2 == 0:
-        evenness = 'even'
-    else:
-        evenness = 'odd'
+        stat = 'even'
     return render_template('6-number_odd_or_even.html', n=n,
-                           evenness=evenness)
+                           stat=stat)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
