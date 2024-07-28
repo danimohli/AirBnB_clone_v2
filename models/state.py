6fe44207ba18550217cada6a1aff1a3f82fa51e8
danimohli <danimohlife@gmail.com>
2024-07-28 01:53:39 +0100

    db

--- models/state.py
+++ models/state.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
+"""
+State class for managing State objects.
+"""
+
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import BaseModel, Base
+from models.base_model import Base, BaseModel
+from models.city import City
 
 
 class State(BaseModel, Base):
     """
-    State class that inherits from BaseModel and Base.
+    Represents a state.
     """
+
     __tablename__ = 'states'
-
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete, delete-orphan")
+    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
 
-    @property
     def cities(self):
         """
-        Getter attribute in case of FileStorage
+        Returns a list of City objects linked to the current State.
         """
-        return [city for city in models.storage.all(City).values()
-                if city.state_id == self.id]
+        if isinstance(storage, DBStorage):
+            return [city for city in storage.all(City).values() if city.state_id == self.id]
+        else:
+            return [city for city in storage.all(City).values() if city.state_id == self.id]
