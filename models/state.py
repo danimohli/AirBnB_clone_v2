ca1a481a56ddf53046db653ea511589ed89169dd
danimohli <danimohlife@gmail.com>
2024-07-28 20:43:33 +0100

    state

--- models/state.py
+++ models/state.py
@@ -1,34 +1,43 @@
 #!/usr/bin/python3
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""
+This module defines the State class which represents a state.
+"""
+import models
 from models.base_model import BaseModel, Base
 from models.city import City
-import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """
-    State class that inherits from BaseModel and Base.
+    Represents a state in the database or file storage.
     """
-    __tablename__ = 'states'
 
-    name = Column(String(128), nullable=False)
     if models.storage_t == "db":
-        cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
     else:
         name = ""
 
     def __init__(self, *args, **kwargs):
         """
-        initializes state
+        Initializes a new state.
         """
         super().__init__(*args, **kwargs)
 
-    @property
-    def cities(self):
-        """
-        Getter attribute in case of FileStorage
-        """
-        if models.storage_t == 'db':
-            return self.cities
-        return [city for city in models.storage.all(City).values() if city.state_id == self.id]
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """
+            Returns a list of City instances related to the state.
+            """
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
